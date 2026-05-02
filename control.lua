9e5b09bf5bb182059b948b2d5e2872c84dac0cd7
Tyler Gemora <tylergemora@yahoo.com>
2026-05-02 16:19:24 -0700

    Setup shortcut and boilerplate code for control.lua.

---
+++ control.lua
@@ -0,0 +1,35 @@
+script.on_init(function()
+
+end)
+
+script.on_event(defines.events.on_player_created, function(event)
+
+end)
+
+script.on_event(defines.events.on_player_removed, function(event)
+
+end)
+
+script.on_event(defines.events.on_gui_click, function(event)
+
+end)
+
+script.on_event(defines.events.on_gui_value_changed, function(event)
+
+end)
+
+script.on_event(defines.events.on_gui_text_changed, function(event)
+
+end)
+
+script.on_event(defines.events.on_lua_shortcut, function(event)
+
+end)
+
+script.on_event(defines.events.on_gui_closed, function(event)
+
+end)
+
+script.on_configuration_changed(function(config_changed_data)
+
+end)
---
+++ data.lua
@@ -0,0 +1,13 @@
+data:extend({
+    {
+      type = "shortcut",
+      name = "blueprint-generation",
+      action = "lua",
+      localised_name = {"bpgn.shortcut_name"},
+      style = "blue",
+      icon = "__blueprint-generation__/graphics/icons/shortcut-toolbar/mip/blueprint-generation-x56.png",
+      icon_size = 56,
+      small_icon = "__blueprint-generation__/graphics/icons/shortcut-toolbar/mip/blueprint-generation-x24.png",
+      small_icon_size = 24
+    }
+})
